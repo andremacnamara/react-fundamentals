e847cc1eb9b1ae999177249c579558cf47f4f078
Andre MacNamara <andremacnamara12@gmail.com>
2021-01-10 16:31:21 +0000

    Extra credit 01 - Use Ref

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,18 +4,20 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  
+
+  const usernameInput = React.useRef(null);
+
   function handleSubmit(e){
     e.preventDefault();
-    const username = e.target.elements.usernameInput.value;
-    onSubmitUsername(username);
+    // const username = e.target.elements.usernameInput.value;
+    onSubmitUsername(usernameInput.current.value);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="usernameInput"/>
+        <input type="text" id="usernameInput" ref={usernameInput}/>
       </div>
       <button type="submit">Submit</button>
     </form>
