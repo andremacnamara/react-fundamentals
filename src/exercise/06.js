bd8b5df10ecc1265c5f623a92b9d874f3287b491
Andre MacNamara <andremacnamara12@gmail.com>
2021-01-10 16:53:20 +0000

    Extra credit = Lowercase valid

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,6 +6,7 @@ import * as React from 'react'
 function UsernameForm({onSubmitUsername}) {
 
   const usernameInput = React.useRef(null);
+  const [error, setError] = React.useState();
 
   function handleSubmit(e){
     e.preventDefault();
@@ -13,13 +14,20 @@ function UsernameForm({onSubmitUsername}) {
     onSubmitUsername(usernameInput.current.value);
   }
 
+  function handleChange(e){
+    const { value } = e.target;
+    const isValid = value === value.toLowerCase();
+    setError( isValid ? null : 'Username must be lowercase');
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="usernameInput" ref={usernameInput}/>
+        <input type="text" id="usernameInput" ref={usernameInput} onChange={handleChange}/>
       </div>
-      <button type="submit">Submit</button>
+      <div style={{ color: 'red' }}>{error}</div>
+      <button disabled={Boolean(error)} type="submit">Submit</button>
     </form>
   )
 }
